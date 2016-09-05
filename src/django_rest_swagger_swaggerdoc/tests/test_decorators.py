aafdf8380fba00d1e9ef324df5db6dd72fe4e92b
TakesxiSximada <takesxi.sximada@gmail.com>
2016-09-05 18:16:23 +0900

    Mock object cannot be passed to the argument of the functools.wraps()

--- src/django_rest_swagger_swaggerdoc/tests/test_decorators.py
+++ src/django_rest_swagger_swaggerdoc/tests/test_decorators.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from . import compat
-
 
 class SwaggerDocDecoratorTest(unittest.TestCase):
     def _get_target(self):
@@ -10,9 +8,12 @@ class SwaggerDocDecoratorTest(unittest.TestCase):
         return target
 
     def test_it(self):
+
+        def _test_fview_func(request):
+            pass
+
         target = self._get_target()
         deco = target('./swagger_test_doc.yml')
-        func = compat.mock.Mock()
-        wrapped_func = deco(func)
+        wrapped_func = deco(_test_fview_func)
         swagger_doc = getattr(wrapped_func, '__swaggerdoc__')
         self.assertIsInstance(swagger_doc, dict)
