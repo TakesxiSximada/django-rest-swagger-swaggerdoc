4056be1eb3091661fa059b08df27edab2467ebad
TakesxiSximada <takesxi.sximada@gmail.com>
2016-09-22 11:13:25 +0900

    cleanup

--- src/django_rest_swagger_swaggerdoc/__init__.py
+++ src/django_rest_swagger_swaggerdoc/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from .decorators import swaggerdoc
+from .renderers import SwaggerAdditinalDocRenderer
 
 __version__ = '1.0.post2'
 
-
 __all__ = [
-    'swaggerdoc'
+    'swaggerdoc',
+    'SwaggerAdditinalDocRenderer',
 ]
